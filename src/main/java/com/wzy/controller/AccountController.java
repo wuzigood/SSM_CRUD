87b86181518c98b260146e0ce18688da29631a14
wuzeyu <31343389+wuzigood@users.noreply.github.com>
2020-02-20 22:38:03 +0800

    添加spring aop事务管理并测试

--- src/main/java/com/wzy/controller/AccountController.java
+++ src/main/java/com/wzy/controller/AccountController.java
@@ -7,6 +7,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.util.List;
 
 @Controller
@@ -24,6 +26,13 @@ public class AccountController {
         List<Account> list = accountService.findAll();
         model.addAttribute("list",list);
         return "list";
+    }
+
+    @RequestMapping("/save")
+    public void save(Account account, HttpServletRequest request, HttpServletResponse response) throws Exception{
+        accountService.saveAccount(account);
+        response.sendRedirect(request.getContextPath()+"/account/findAll");
+        return ;
     }
 
 }
